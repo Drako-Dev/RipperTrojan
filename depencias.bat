4b1da8d3a7fadb088867558242132c6a8440c65f
Salim <83632722+Dr4k0D3v@users.noreply.github.com>
2021-07-06 11:18:14 -0300

    Update depencias.bat

--- depencias.bat
+++ depencias.bat
@@ -1,5 +1,5 @@
 @echo OFF
-echo [RIPPER] Dependencies intaller 1.1
+echo [RIPPER] Dependencies intaller 1.2
 echo By: __Drako__
 pause
 python -m pip install --upgrade pip
@@ -8,4 +8,5 @@ pipwin install pyaudio
 pip install vidstream
 pip install Pillow
 pip install keyboard
-
+pip install base64
+pip install pynput
